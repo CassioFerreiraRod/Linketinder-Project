a408f836a1b8b15887abc512bdb8a11a5f8fc826
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-02-24 15:22:39 -0300

    alterando atributos da classe Pessoa

--- src/br/com/linketinder/pessoa/Pessoa.groovy
+++ src/br/com/linketinder/pessoa/Pessoa.groovy
@@ -1,14 +1,15 @@
 package br.com.linketinder.pessoa
 
 class Pessoa {
-    String nome, email, cep, estado
+    String nome, email, cep, estado, descricao
     List<String> competencias = []
 
-    Pessoa(String nome, String email, String cep, String estado, List<String> competencias) {
+    Pessoa(String nome, String email, String cep, String estado, List<String> competencias, String descricao) {
         this.nome = nome
         this.email = email
         this.cep = cep
         this.estado = estado
         this.competencias = competencias
+        this.descricao = descricao
     }
 }
