2927f14d3cae4ee4f866261fc5c6ea3be9d02405
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-02-23 21:05:31 -0300

    classe Candidato

---
+++ src/br/com/linketinder/pessoa/candidato/Candidato.groovy
@@ -0,0 +1,30 @@
+package br.com.linketinder.pessoa.candidato
+
+import br.com.linketinder.pessoa.Pessoa
+
+class Candidato extends Pessoa {
+    String descricaoPessoal, cpf
+    int idade
+
+    Candidato(String nome, String email, String cep, String estado, List<String> competencias, String descricaoPessoal, String cpf, int idade) {
+        super(nome, email, cep, estado, competencias)
+        this.descricaoPessoal = descricaoPessoal
+        this.cpf = cpf
+        this.idade = idade
+    }
+
+
+    @Override
+    public String toString() {
+        return """\
+    Nome: ${super.nome},
+    Email: ${super.email},
+    CPF:'$cpf',
+    Idade:$idade,
+    Estado: ${super.estado},
+    Cep: ${super.cep},
+    Descricao Pessoal:'$descricaoPessoal',
+    Competências: ${super.competencias}
+}"""
+    }
+}
