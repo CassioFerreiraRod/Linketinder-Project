9bdfa4c387c6e6431f4749d3526b385681a330e4
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-02-24 15:23:19 -0300

    alterando construtor e toString() da classe Candidato

--- src/br/com/linketinder/pessoa/candidato/Candidato.groovy
+++ src/br/com/linketinder/pessoa/candidato/Candidato.groovy
@@ -3,16 +3,14 @@ package br.com.linketinder.pessoa.candidato
 import br.com.linketinder.pessoa.Pessoa
 
 class Candidato extends Pessoa {
-    String descricaoPessoal, cpf
+    String  cpf
     int idade
 
-    Candidato(String nome, String email, String cep, String estado, List<String> competencias, String descricaoPessoal, String cpf, int idade) {
-        super(nome, email, cep, estado, competencias)
-        this.descricaoPessoal = descricaoPessoal
+    Candidato(String nome, String email, String cep, String estado, List<String> competencias, String descricao, String cpf, int idade) {
+        super(nome, email, cep, estado, competencias, descricao)
         this.cpf = cpf
         this.idade = idade
     }
-
 
     @Override
     public String toString() {
@@ -24,7 +22,7 @@ class Candidato extends Pessoa {
     Idade:$idade,
     Estado: ${super.estado},
     Cep: ${super.cep},
-    Descricao Pessoal: $descricaoPessoal,
+    Descricao Pessoal: ${super.descricao},
     Competências: ${super.competencias}
 }"""
     }
