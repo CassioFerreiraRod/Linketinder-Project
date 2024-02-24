e630d34e8d82fa857edbf54c34007d445d73e086
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-02-24 15:24:07 -0300

    alterando diretório da classe Main

---
+++ src/Main.groovy
@@ -0,0 +1,9 @@
+import br.com.linketinder.menu.Menu
+
+// Cassio Ferreira Rodrigues
+class Main {
+    static void main(String[] args) {
+        Menu menu = new Menu()
+        menu.exibirMenu()
+    }
+}
