d8c5cd835d67cb6f70521e23d974de8f817dcb1b
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-03-29 20:53:29 -0300

    Atualizando a classe Menu para exibir menu candidato e listar

--- src/main/groovy/br/com/linketinder/view/Menu.groovy
+++ src/main/groovy/br/com/linketinder/view/Menu.groovy
@@ -1,11 +1,12 @@
 package br.com.linketinder.view
 
+import br.com.linketinder.dao.CandidatoDAO
 import br.com.linketinder.model.entity.Cadastro
 
 
 class Menu {
 
-    Cadastro cadastro = new Cadastro()
+    CandidatoDAO candidatoDAO = new CandidatoDAO()
 
     void exibirMenu() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
@@ -15,17 +16,25 @@ class Menu {
 
         while (continuar) {
             println "Escolha uma das opções a seguir"
-            println "1. Listar candidatos"
-            println "2. Listar empresas"
+            println "1. Exibir menu Candidatos"
+            println "2. Exibir menu empresas"
+            println "3. Exibir menu vagas"
+            println "4. Exibir menu competências"
             println "0. Para sair"
             opcao = br.readLine()
 
             switch (opcao) {
                 case '1':
-                    cadastro.listarCandidatos()
+                   menuCandidatos(br)
                     break
                 case '2':
-                    cadastro.listarEmpresas()
+
+                    break
+                case '3':
+
+                    break
+                case '4':
+
                     break
                 case '0':
                     continuar = false
@@ -37,5 +46,42 @@ class Menu {
 
             }
         }
+    }
+
+   void menuCandidatos(BufferedReader bufferedReader) {
+        boolean continuar = true
+       def opcao
+       while (continuar) {
+           println("Escolha uma das opções a seguir:")
+           println("1. Listar Todos os Candidatos")
+           println("2. Cadastrar Candidato")
+           println("3. Atualizar Candidato")
+           println("4. Excluir Candidato")
+           println("0. Voltar")
+           opcao = bufferedReader.readLine()
+
+           switch (opcao) {
+               case '1':
+                   println candidatoDAO.listar()
+                   break
+               case '2':
+
+                   break
+               case '3':
+
+                   break
+               case '4':
+
+                   break
+               case '0':
+                   continuar = false
+                   println "Voltando..."
+                   break
+               default:
+                   println "Opção inválida"
+                   break
+
+           }
+       }
     }
 }
