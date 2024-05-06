82496feb465256a3751feccccc79ed26ad296b9c
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-05-06 01:15:44 -0300

    adicionando endpoint de cadastro ao CandidatoController

--- src/main/groovy/br/com/linketinder/controller/CandidatoController.groovy
+++ src/main/groovy/br/com/linketinder/controller/CandidatoController.groovy
@@ -1,9 +1,45 @@
 package br.com.linketinder.controller
 
+import br.com.linketinder.model.entity.Candidato
 import br.com.linketinder.service.CandidatoService
+import br.com.linketinder.service.CompetenciaService
 
+import javax.ws.rs.Consumes
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.WebApplicationException
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import java.util.logging.Level
+import java.util.logging.Logger
+
+@Path("/candidatos")
 class CandidatoController {
 
-    CandidatoService candidatoService
+    private CandidatoService candidatoService = new CandidatoService()
+    private CompetenciaService competenciaService = new CompetenciaService()
+    private Response response
+    private Map mensagem = [
+            "status": 201,
+            "mensagem": "Cadastro feito com sucesso"
+    ]
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/")
+    Response cadastrarCandidato(Candidato candidato) {
+        try {
+            candidatoService.cadastrarCandidato(candidato)
+            competenciaService.cadastrarCandidatoCompetencia(candidato.getCompetencias())
+            response = Response.status(Response.Status.CREATED)
+                    .entity(mensagem)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build()
+            return response
+        } catch (WebApplicationException ex) {
+            Logger.getLogger(CandidatoController.class.getName()).log(Level.SEVERE, null, ex)
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR)
+        }
+    }
 
 }
