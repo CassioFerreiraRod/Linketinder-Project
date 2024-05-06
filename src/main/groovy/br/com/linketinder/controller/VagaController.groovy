abeef4c252adaac557218445bab0bfd3257a6416
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-05-06 01:19:16 -0300

    adicionando endpoint de cadastro ao VagaController

--- src/main/groovy/br/com/linketinder/controller/VagaController.groovy
+++ src/main/groovy/br/com/linketinder/controller/VagaController.groovy
@@ -1,9 +1,39 @@
 package br.com.linketinder.controller
 
+import br.com.linketinder.model.entity.Vaga
+import br.com.linketinder.service.CompetenciaService
 import br.com.linketinder.service.VagaService
 
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import java.util.logging.Level
+import java.util.logging.Logger
+
+@Path("/empresas/vagas")
 class VagaController {
 
-    VagaService vagaService
+    private VagaService vagaService = new VagaService()
+    private CompetenciaService competenciaService = new CompetenciaService()
+    private Map mensagem = [
+            resposta: Response.Status.CREATED
+    ]
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/")
+    Response cadastrarVaga(Vaga vaga) {
+        try {
+            vagaService.cadastrarVaga(vaga)
+            competenciaService.cadastrarVagaCompetencia(vaga.getCompetencias())
+
+            return Response.status(Response.Status.CREATED)
+                    .entity(mensagem)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build()
+        } catch (WebApplicationException ex) {
+            Logger.getLogger(VagaController.class.getName()).log(Level.SEVERE, null, ex)
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR)
+        }
+    }
 }
